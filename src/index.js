350603fae2b5aa45a16b0f834c8006aceb73addb
Ivan Amoshyi <amoshyiivan@gmail.com>
2022-08-17 22:44:42 +0300

    добавил шаблон

--- src/index.js
+++ src/index.js
@@ -1,22 +1,10 @@
 import items from "./gallery-items.js";
+import template from './templates/gallery.hbs';
+
+
 
 function createHtml(items) {
-  return items.map(({ preview, original, description }) => {
-      return `<li class="gallery__item">
-        <a
-            class="gallery__link"
-            href="${original}"
-        >
-            <img
-                class="gallery__image"
-                src="${preview}"
-                data-source="${original}"
-                alt="${description}"
-            />
-        </a>
-    </li>`;
-    })
-    .join("");
+  return template(items)
 }
 
 const gallery = document.querySelector('.js-gallery');
